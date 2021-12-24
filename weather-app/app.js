2d99a1c6ad8d0979c05c3bff82a975d4a8e46c8b
Michael McLean <g4m3rm1k3@hotmail.com>
2021-12-24 04:06:42 -0500

    added weather app first commit

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log("Starting");
+
+setTimeout(() => {
+	console.log("2 Second timer");
+}, 2000);
+
+setTimeout(() => {
+	console.log("0 second Timer");
+}, 0);
+
+console.log("Stopping");
